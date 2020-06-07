0a56408ed9964f8ffac45975a4d3e8ed169ca81b
Yazan <yazansh7@hotmail.com>
2020-06-07 10:43:12 +0300

    cleanup

--- automated.py
+++
@@ -1,8 +0,0 @@
-from scrapy.crawler import CrawlerProcess
-from TweetScraper.spiders.TweetCrawler import TweetScraper
-
-process = CrawlerProcess()
-process.crawl(TweetScraper, query="(corona OR virus OR pandemic OR coronavirus OR covid-19 OR covid19) near:nyc until:2020-03-31 since:2020-03-10")
-process.start()
-
-
